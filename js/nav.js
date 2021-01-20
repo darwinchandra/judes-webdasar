a0cb6f864c86c2b4a5f8f0982a67cfab77009325
darwinchandra <darwin.chandra0@gmail.com>
2021-01-20 23:23:05 +0700

    change1

--- js/nav.js
+++ js/nav.js
@@ -63,3 +63,10 @@ hamburger.addEventListener('click', () => {
       0
     );
 });
+
+function myFunction(e) {
+  if (document.querySelector('#navList a.active') !== null) {
+    document.querySelector('#navList a.active').classList.remove('active');
+  }
+  e.target.className = 'active';
+}
